e211b99795031228ee61162fc540e8ca9ccae8e3
Orlando Brea <orlando.brea@gmail.com>
2019-03-29 10:10:08 -0300

    Finalizada

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ var myErrorHandler = function(error, req, res, next) {
 
 // La informacion enviada por POST de un formulario se recibe en req.body
 app.use(body_parser.urlencoded({extended: false}));
+app.use(body_parser.json());
 
 
 app.use(myLogger);
@@ -85,6 +86,76 @@ app.get('/listar', function(req, res, next) {
 // Comienzo de la aplicacion
 
 app.use('/artistas', require('./controllers/artistaControllers'));
+
+// API de artistas /api/artistas
+
+/**
+ * API de Artistas
+ *  GET /api/artistas -> listado de todos los artistas
+ *  GET /api/artistas/:id -> Obtener el artista cuyo ID es pasado como parametro
+ *  POST /api/artistas -> Agregar un artista
+ *  PUT /api/artistas/:id -> Actualizar los datos del artista cuyo ID es pasado como parametro
+ *  DELETE /api/artistas/:id -> Borrar el artista cuyo ID es pasado como parametros
+ */
+
+
+// Listado de artistas
+app.get('/api/artistas', function(req, res, next) {
+    // Es el mismo codigo del listado de artistas normal (pero sin interfaz)
+    ArtistaModel.find({}, (err, listado) => {
+        if (err) {
+            next(new Error('No se pudieron obtener los artistas'));
+            return;
+        }
+        res.send(listado);
+    });
+});
+
+// Obtener un artista
+app.get('/api/artistas/:id', function (req, res, next) {
+    var idArtista = req.params.id;
+    ArtistaModel.findById(idArtista, (err, artista) => {
+        if (err) {
+            next(new Error('No se encontro el artista'));
+        }
+        res.send(artista);
+    });
+});
+
+// Agregar un artista
+app.post('/api/artistas', function (req, res, next) {
+    var nombre = req.body.nombre;
+    var instancia = new ArtistaModel({ nombre: nombre });
+    instancia.save((err, artista) => {
+        if (err) {
+            next(new Error('No se pudo guardar el artista'));
+        }
+        res.status(201).send(artista); // Codigo HTTP 201 es creado
+    });
+});
+
+// Actualizar un artista
+app.put('/api/artistas/:id', function (req, res, next) {
+    var idArtista = req.params.id;
+    var nombreNuevo = req.body.nombre;
+    ArtistaModel.findByIdAndUpdate(idArtista, { nombre: nombreNuevo }, (err, artista) => {
+        if (err) {
+            next(new Error('No se pudo actualizar el artista'));
+        }
+        res.send(artista);
+    })
+});
+
+// Borrar un artista
+app.delete('/api/artistas/:id', function (req, res, next) {
+    var idArtistaABorrar = req.params.id;
+    ArtistaModel.findByIdAndRemove(idArtistaABorrar, (err, artista) => {
+        if (err) {
+            next(new Error('No se pudo borrar el artista'));
+        }
+        res.status(204).send(); // Codigo HTTP 204 Sin contenido (no hay nada que responder, pero la operacion fue exitosa)
+    });
+});
 
 app.use(myErrorHandler);
 
