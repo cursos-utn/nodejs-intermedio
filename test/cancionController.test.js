632ad3e4f61a196883f46bfcebadd7d9f373aefc
Orlando Brea <orlando.brea@gmail.com>
2019-08-26 11:08:54 -0300

    Version de testeo

--- test/cancionController.test.js
+++ test/cancionController.test.js
@@ -19,11 +19,26 @@ describe('Pagina principal', () => {
 
     it('Obtener pagina principal', (done) => {
         chai.request(server).get('/').end( (err, res) => {
+            console.log(res.text);
             res.should.have.status(200);
             done();
         })
     })
 
+    it('Listado de artistas', (done) => {
+        chai.request(server).get('/api/artistas').end( (err, res) => {
+            //console.log(res);
+            res.should.have.status(200);
+            done();
+        } )
+    });
 
+    it('Agregar un artista', (done) => {
+        chai.request(server).post('/api/artistas').send({nombre: 'Artista A'}).end((err, res) => {
+            //console.log(res);
+            res.should.have.status(201);
+            done();
+        }) 
+    })
 })
 
